8d9b467925f79d1d56a0d1d30b11fe4573883b91
Lukas Batrla <s180012@student.dtu.dk>
2018-11-24 13:15:32 +0100

    Add method to create databases

--- db/src/StockRegistry.py
+++ db/src/StockRegistry.py
@@ -2,66 +2,72 @@ import sqlite3
 import os
 
 class StockRegistry:
-    # Initialize the CarRegistry. The name of the database file used for
+    # Initialize the StockRegistry. The name of the database file used for
     # this instance is given as a parameter. This database will in some
     # cases be pre populated, and in some cases empty.
     # You do not need to change this function.
     def __init__(self, databaseName):
         self.db = sqlite3.connect(databaseName)
-    
+        self.c = self.db.cursor()
+        self.createTables()
+
     # Clean up the database. You do not need to change this function.
     def close(self):
         self.db.close()
-        
-    
+
+    # This function should create two tables stocks and quotes.
+    # You can assume the tables do not exist.
+    def createTables(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS stocks
+            ( stock_id INTEGER NOT NULL, name TEXT NOT NULL UNIQUE, is_etf INTEGER NOT NULL, PRIMARY KEY(stock_id) )''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS quotes
+            ( quote_id INTEGER NOT NULL UNIQUE, date TEXT NOT NULL, close REAL NOT NULL, stock_id NUMERIC NOT NULL, FOREIGN KEY(stock_id) REFERENCES stocks(stock_id), PRIMARY KEY(quote_id) )''')
+        self.db.commit()
+
     # Add a row to the models table.
     def addStock(self, stockName, isETF):
-        c = self.db.cursor()
-        c.execute('INSERT INTO stocks( name, is_etf) VALUES (?,?)', [stockName,isETF])
+        self.c.execute('INSERT INTO stocks( name, is_etf) VALUES (?,?)', [stockName,isETF])
         self.db.commit()
-        
+
     def updateStock(self, stockName, isETF):
-        c = self.db.cursor()
-        c.execute('UPDATE stocks set is_etf=(?) where name=(?)',[isETF,stockName])
+        self.c.execute('UPDATE stocks set is_etf=(?) where name=(?)',[isETF,stockName])
         self.db.commit()
-        
+
     def addManyStocks(self, rows):
         try:
-            c = self.db.cursor()
-            c.executemany('INSERT INTO stocks( name, is_etf) VALUES (?,?)', rows)
+            self.c.executemany('INSERT INTO stocks( name, is_etf) VALUES (?,?)', rows)
             self.db.commit()
         except sqlite3.IntegrityError as ie:
             print ('addManyStocks ie:'+str( ie))
         except sqlite3.Error as er:
             print ('addManyStocks er:'+str( er))
             self.db.close()
-        
+
     def addManyQuotes(self, rows):
         try:
-            c = self.db.cursor()
-            c.executemany('INSERT INTO quotes( date, open, high, low, close, volume, open_int, stock_id) VALUES (?,?,?,?,?,?,?,?)', rows)
+            self.c.executemany('INSERT INTO quotes( date, close, stock_id) VALUES (?,?,?)', rows)
             self.db.commit()
         except sqlite3.Error as er:
             print ('addManyQuotes er:'+str( er))
             self.db.close()
     # Add a row to the registration table.
-    def addQuote(self, date, openValue,high,low,close, volumne, openInd, stockId):
-        c = self.db.cursor()
-        c.execute('INSERT INTO quotes VALUES (?,?,?,?,?,?,?,?)', [date,openValue,high,low,close, volumne, openInd, stockId])
+    def addQuote(self, date, openValue, high, low, close, volumne, openInd, stockId):
+        self.c.execute('INSERT INTO quotes VALUES (?,?,?)', [date, close, stockId])
         self.db.commit()
-    
+
     # Delete the row with the given carId from the registration table.
     def getStockId(self, stockName):
-        c = self.db.cursor()
-        return c.execute('Select stock_id FROM stocks WHERE name = (?)', [stockName]).fetchone()[0]
-    
+        return self.c.execute('Select stock_id FROM stocks WHERE name = (?)', [stockName]).fetchone()[0]
+
     def getTop10(self, table_name):
-        c = self.db.cursor()
-        return c.execute('Select top 10 * FROM (?)', [table_name]).fetchall()
+        return self.c.execute('SELECT * FROM %s LIMIT 10' % table_name).fetchall()
+
+    def numOfRows(self, table_name):
+        return self.c.execute('SELECT COUNT(*) FROM %s' % table_name).fetchone()
 
 
     # Populate the registration and models tables with meaningful data.
-    # Ie. carIds must be unique, for any modelId in registration there 
+    # Ie. carIds must be unique, for any modelId in registration there
     # should be a corresponding row in models, etc. You must add at least
     # 10 rows to each of the two tables.
     #def addManyStocks(self, stocks):
@@ -69,7 +75,6 @@ class StockRegistry:
         #c.executemany('INSERT INTO models VALUES (?,?,?)', models)
         #c.executemany('INSERT INTO registrations VALUES (?,?,?,?)', registrations)
         #self.db.commit()
-                
+
     def deleteDatabase(self, databaseName):
         os.remove(databaseName)
-    
--- db/src/import_data.py
+++ db/src/import_data.py
@@ -6,52 +6,57 @@ import glob, os
 
 # 1. Set database dictionary - it will be created there!
 
-os.chdir(r"C:\Users\Kamil\Desktop\DTU sem 2\02807_Computational_Tools_for_Data_Science\project\db")
+os.chdir(r"..")
 
 # 2. pcik name of dictionary !
-registry = StockRegistry("stock_database.db")
+registry = StockRegistry("stock_database.sqlite")
+registry.createTables()
 
 # 3 import all stocks to the database:
 # Set dir with Stocks
-os.chdir("..\Data\Stocks")
+os.chdir("data/Stocks")
 stocks =  glob.glob("*.txt")
 rows = []
 for file in stocks:
-    stock_short_name = file.split(".",1)[0]
-    rows.append([stock_short_name,0])
+    if os.path.getsize(file) > 0:
+        stock_short_name = file.split(".",1)[0]
+        rows.append([stock_short_name,0])
 registry.addManyStocks(rows)
     #print(stock_short_name)
     #registry.addStock(stock_short_name,0)
-    
+
 
 for file in stocks:
-    stock_short_name = file.split(".",1)[0]
-    stockId = registry.getStockId(stock_short_name)
-    quotes = pd.read_csv(file,sep=",")
-    quotes['stock_id']=stockId
-    tuples = [tuple(x) for x in quotes.values]
-    registry.addManyQuotes(tuples)
+    if os.path.getsize(file) > 0:
+        stock_short_name = file.split(".",1)[0]
+        stockId = registry.getStockId(stock_short_name)
+        quotes = pd.read_csv(file,sep=",", usecols=[0, 4])
+        quotes['stock_id']=stockId
+        tuples = [tuple(x) for x in quotes.values]
+        registry.addManyQuotes(tuples)
 
 
-# 4 import all ETFs to the database: 
+# 4 import all ETFs to the database:
 # Set dir with ETF
-os.chdir("..\ETFs")
+os.chdir("../ETFs")
 etfs =  glob.glob("*.txt")
 rows = []
 for file in etfs:
-    etf_short_name = file.split(".",1)[0]
-    rows.append([etf_short_name,1])
+    if os.path.getsize(file) > 0:
+        etf_short_name = file.split(".",1)[0]
+        rows.append([etf_short_name,1])
 registry.addManyStocks(rows)
 
-    
+
 for file in etfs:
-    etf_short_name = file.split(".",1)[0]
-    stockId = registry.getStockId(etf_short_name)
-    quotes = pd.read_csv(file,sep=",")
-    quotes['stock_id']=stockId
-    tuples = [tuple(x) for x in quotes.values]
-    registry.addManyQuotes(tuples)
-    
+    if os.path.getsize(file) > 0:
+        etf_short_name = file.split(".",1)[0]
+        stockId = registry.getStockId(etf_short_name)
+        quotes = pd.read_csv(file,sep=",", usecols=[0, 4])
+        quotes['stock_id']=stockId
+        tuples = [tuple(x) for x in quotes.values]
+        registry.addManyQuotes(tuples)
+
 registry.close()
 
 #for file in etfs:
